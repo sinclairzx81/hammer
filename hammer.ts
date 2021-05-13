e91164cb431ef6e07365789c17f31f18a912e96d
sinclairzx81 <haydn.developer@gmail.com>
2021-05-14 03:18:44 +0900

    updates

--- hammer.ts
+++ hammer.ts
@@ -1,9 +1,3 @@
-import { shell } from './src/system/index'
-
 export async function start(input: string) {
-    await shell([
-        'hammer watch hammer.ts --dist dist/inner',
-        'hammer watch hammer.ts --dist dist/outer'
-    ])
     console.log('hello world', input)
 }
